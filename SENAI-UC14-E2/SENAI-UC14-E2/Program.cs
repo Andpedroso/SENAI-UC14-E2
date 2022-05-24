af57f05ed139ad432dd78af6d1418d736968e56b
Andpedroso <ankpedroso@gmail.com>
2022-05-23 22:53:41 -0300

    API com CRUD completo

---
+++ SENAI-UC14-E2/SENAI-UC14-E2/Program.cs
@@ -0,0 +1,32 @@
+using SENAI_UC14_E2.Contexts;
+using SENAI_UC14_E2.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<ChapterContext, ChapterContext>();
+
+builder.Services.AddTransient<LivroRepository, LivroRepository>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
